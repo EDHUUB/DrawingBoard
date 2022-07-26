107a29d556d3b8bf84b946ffa20ddc655510524c
EDHUUB <1984528378@qq.com>
2022-07-26 15:20:54 +0800

    [2022/7/26] 添加画笔及绘制 done: 1、实现简易单笔触绘制
    todo:
    1、方法draw的原理，逻辑
    2、学习监听器原理与使用

--- app/src/main/java/com/xpw/drawingboard/DrawView.java
+++ app/src/main/java/com/xpw/drawingboard/DrawView.java
@@ -2,24 +2,44 @@ package com.xpw.drawingboard;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 
-public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
+public class DrawView extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+
+    //画笔
+    private Paint paint = new Paint();
+
+    //路径
+    private Path path = new Path();
 
 
+    //初始化
     public DrawView(Context context, AttributeSet attrs) {
         super(context,attrs);
-
         getHolder().addCallback(this);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(10);
+        paint.setStyle(Paint.Style.STROKE);
+        //初始化onTouchListener
+        setOnTouchListener(this);
     }
 
+    //绘制动作
+    //todo:原理，逻辑
     public void draw(){
         Canvas canvas = getHolder().lockCanvas();
 
+        canvas.drawColor(Color.GRAY);
+        canvas.drawPath(path,paint);
 
         getHolder().unlockCanvasAndPost(canvas);
     }
@@ -37,5 +57,27 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
 
+    }
+
+
+    //todo：监听器原理与使用
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        switch ( event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(event.getX(),event.getY());
+                draw();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(event.getX(),event.getY());
+                draw();
+                break;
+
+        }
+
+
+        return true;
     }
 }
