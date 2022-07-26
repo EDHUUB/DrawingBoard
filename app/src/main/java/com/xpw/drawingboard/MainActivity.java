88de067a2933c781bfaa5cd7285d08df35161859
EDHUUB <1984528378@qq.com>
2022-07-26 11:53:03 +0800

    简易白板项目开发

---
+++ app/src/main/java/com/xpw/drawingboard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xpw.drawingboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
