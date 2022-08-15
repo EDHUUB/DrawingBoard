53bbfdaead58a096503dedeb1dc653da48c1bf60
EDHUUB <1984528378@qq.com>
2022-08-15 12:03:46 +0800

    [2022/8/15] 添加pointer类注释
    done:
    1、添加pointer类注释
    todo
    1、pointerTemp的初始化可以抽出一个方法
    2、橡皮擦属性与部分方法的抽取
    3、实现贝塞尔曲线

--- app/src/main/java/com/xpw/drawingboard/pojo/Pointer.java
+++ app/src/main/java/com/xpw/drawingboard/pojo/Pointer.java
@@ -5,6 +5,14 @@ import android.graphics.Path;
 import lombok.Data;
 
 @Data
+/**
+ * 1、id用于存储pointerId，用于后期查询目标pointer的属性，如preX、X
+ * 2、path记录pointer的path，由于在move事件中，只被Path new一次出来的path会被不断更改存储地址，所以需要一个中间变量存储path数值
+ * 3、x记录pointer当前的x坐标
+ * 4、y记录pointer当前的y坐标
+ * 5、preX记录pointer之前的x坐标，配合x属性实现贝塞尔曲线和字迹功能
+ * 6、preY记录pointer之前的Y坐标,配合y属性实现贝塞尔曲线和字迹功能
+ */
 public class Pointer {
 
     private int id;
